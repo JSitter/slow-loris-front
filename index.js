8548a8aae166739bd14adefb9dbf808f1a627140
Justin Sitter <justin@jaytria.com>
2018-04-18 15:07:23 -0700

    updated jade call

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.static('./public'))    // set directory for static files
 port = process.env.PORT || 5000
 
 app.set('views', __dirname + '/views');            // set express view template directory for express
-app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
 app.use(express.static(__dirname+ "/public"))
 
 app.get('/', function (req, res) {
